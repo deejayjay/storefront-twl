b351b6c1238f753f9b4da5067133edb0b98adb9a
Deepak Joy Jose <deejayjay@zoho.com>
2022-03-31 14:43:49 -0600

    Adds the PageTitle component and GlobalStyles

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// To access this file, go to http://localhost:3000/api/hello
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "Deepak Joy Jose" });
 }
